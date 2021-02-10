d29cff5fd4300ac2889c241ffe8b35a7a8da918b
ZAV-IT <78853402+zav-it@users.noreply.github.com>
2021-02-10 20:30:05 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+from loader import bot
 from utils.db_api.add_to__database import start_bd
 from utils.db_api.database import create_datab
 
 create_datab()
 
 start_bd()
+
+
+async def on_shutdown(dp):
+    await bot.close()
 
 
 async def on_startup(dp):
@@ -20,4 +25,4 @@ if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_shutdown=on_shutdown(dp), on_startup=on_startup)
